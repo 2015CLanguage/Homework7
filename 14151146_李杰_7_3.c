c06239fe5171b9c04ee371e0573c111a783277a3
2015CLanguage <buaa_2015_c@163.com>
2015-05-12 15:00:24 +0800

    Create 14151146_李杰_7_3.c

---
+++ 14151146_李杰_7_3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int dis(int x)
+{
+	int i;
+	for(i=2;i<(x/2);i++)
+	{
+		if(x%i==0&&(x!=i))
+			return 1;
+	}
+	return 0;
+}
+void main()
+{
+	int num;
+	char j;
+	while(1)
+	{
+		printf("Please enter a number:");
+		scanf("%d",&num);
+		getchar('\n');
+		if(dis(num))
+			printf("%d不是素数\n",num);
+		else
+			printf("%d是素数\n",num);
+		
+		printf("Continue or not(Y/N)?  ");
+		scanf("%c",&j);
+		if(j=='N'||j=='n')
+			break;
+	}
+}
