bd1a7f965ecd91a49143d892ce05274b297baf34
2015CLanguage <buaa_2015_c@163.com>
2015-05-12 15:20:48 +0800

    Create 14151142_解树聪_7_6.c

---
+++ 14151142_解树聪_7_6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void lianjie(char string1[],char string2[],char string[])
+{
+	int i,j;
+	for(i=0;string1[i]!='\0';i++)
+		string[i]=string1[i];
+    for(j=0;string2[j]!='\0';j++)
+		string[i+j]=string2[j];
+	string[i+j]='\0';
+}
+void main()
+{
+	char s1[100],s2[100],s[200];
+	printf("请输入一串字符串:\n");
+	gets(s1);
+	printf("请输入另一串字符串:\n");
+	gets(s2);
+	lianjie(s1,s2,s);
+	printf("连接后字符串为:\n");
+	puts(s);
+}
