2c4a3c87e5de7657e112366a891e60861e0f7683
2015CLanguage <buaa_2015_c@163.com>
2015-05-12 22:33:57 +0800

    Update 14151145_邱林伟_7_6.c

--- 14151145_邱林伟_7_6.c
+++ 14151145_邱林伟_7_6.c
@@ -1,23 +1,24 @@
-#include<stdio.h>
-void main()
-{
-	void connect(char string1[],char string2[],char string[]);
-	char string1[100],string2[100],string[200];
-    printf("请输入第一串字符串:");
-	scanf("%s",string1);
-	printf("请输入第二串字符串:");
-    scanf("%s",string2);
-	printf("连接的字符串为:\n%s\n",string);
-    connect(string1,string2,string);
-	printf("\n");
-}
-
-void connect(char string1[],char string2[],char string[])
-{
-	int i,j;
-	for(i=0;string1[i]!='\0';i++)
-	string[i]=string1[i];
-	for(j=0;string2[j]!='\0';j++)
-	string[i+j]=string2[j];
+#include<stdio.h> 
+void main() 
+{ 
+	void connect(char string1[],char string2[],char string[]); 
+ 	char string1[100],string2[100],string[200]; 
+         printf("请输入第一串字符串:"); 
+ 	scanf("%s",string1); 
+	printf("请输入第二串字符串:"); 
+        scanf("%s",string2); 
+        connect(string1,string2,string);
+ 	printf("连接的字符串为:\n%s\n",string);    
+ 	printf("\n"); 
+} 
+ 
+void connect(char string1[],char string2[],char string[]) 
+{ 
+ 	int i,j; 
+	for(i=0;string1[i]!='\0';i++) 
+		string[i]=string1[i]; 
+	for(j=0;string2[j]!='\0';j++) 
+		string[i+j]=string2[j];
 	string[i+j]='\0';
 }
+	
