8c1ae55d8e39657f36d594e86bf8b32de71c6a30
2015CLanguage <buaa_2015_c@163.com>
2015-05-12 18:08:56 +0800

    Update 14151193_吴偌灏_7_6.c

--- 14151193_吴偌灏_7_6.c
+++ 14151193_吴偌灏_7_6.c
@@ -1,18 +1,17 @@
 #include<stdio.h>
 #include<string.h>
-void connect(char str1[20]='',char str2[20]='')
+void connect(char str1[],char str2[],char str[])
 {
 	int i,j;
-	char str[40]='';
 	for (i=0;str1[i]!='\0';i++)str[i]=str1[i];
 	for (j=0;str2[j]!='\0';j++)str[i+j]=str2[j];
 	str[i+j]='\0';
-	puts("%s",str[]);
 }
 void main()
 {
-	char str1[20]='',str2[20]='';
-	gets(str1[]);
-	gets(str2[]);
-	connect(str1[],str2[]);
+	char str1[20],str2[20],str[40];
+	scanf("%s",str1);
+	scanf("%s",str2);
+	connect(str1,str2,str);
+	printf("%s",str);
 }
