0d4b8b78d01d47c7e42fc9e86fc68dbf75692ef5
2015CLanguage <buaa_2015_c@163.com>
2015-05-12 13:50:08 +0800

    Create 14151137_于春辉_7_1.c

---
+++ 14151137_于春辉_7_1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int max(int a,int b){
+	int x;
+	if(a<b){
+		x=a;
+		a=b;
+		b=x;
+	}
+	while((x=a%b)!=0){
+		a=b;
+		x=a;
+	}
+	x=b;
+	return x;
+}
+
+int min(int a, int b, int x){
+	int y;
+	y=a*b/x;
+	return y;
+}
+
+int main(){
+	int a,b,x,y;
+	scanf("%d%d",&a,&b);
+	x=max(a,b);
+	y=min(a,b,x);
+	printf("最大公约数为%d，最小公倍数是%d",x,y);
+	return 0;
+} 
