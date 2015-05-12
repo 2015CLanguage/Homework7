387b16240455de11dec24650edb8d294ae39d9b1
2015CLanguage <buaa_2015_c@163.com>
2015-05-12 18:37:09 +0800

    Update 14151146_李杰_7_6.c

--- 14151146_李杰_7_6.c
+++ 14151146_李杰_7_6.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 #include<string.h>
-void link(char a[], char b[],char c[])
+void link(char a[], char b[])
 {
 	strcat(a,b);
 }
 void main()
 {
-	char str1[50],str2[50],str[50];
+	char str1[50],str2[50];
 	printf("Please input str1:");
 	gets(str1);
 	printf("Please input str2:");
 	gets(str2);
-	link(str1,str2,str);
+	link(str1,str2);
 	printf("The linked string: %s\n",str1);
 }
