35f2099a2a22a12d3fada5180d000d6b66809edf
2015CLanguage <buaa_2015_c@163.com>
2015-05-12 14:40:21 +0800

    Create 14151137_于春辉_7_6.c

---
+++ 14151137_于春辉_7_6.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{void C(char str1[],char str2[],char str[]);
+char s1[100],s2[100],s[100];
+printf("input str1:");
+scanf("%s",s1);
+printf("input str2:");
+scanf("%s",s2);
+C(s1,s2,s);
+printf("\n 输出的字符串是 %s\n",s);
+return 0;
+}
+void C(char str1[],char str2[],char str[])
+{int i,j;
+for (i=0;str1[i]!='\0';i++)
+	 str[i]=str1[i];
+for (j=0;str2[j]!='\0';j++)
+	 str[i+j]=str2[j];
+str[i+j]='\0';
+}
