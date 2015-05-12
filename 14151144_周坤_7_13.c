b04825f1f8ea841c9bd720b0f1af43fab2ed3061
2015CLanguage <buaa_2015_c@163.com>
2015-05-12 15:44:17 +0800

    Create 14151144_周坤_7_13.c

---
+++ 14151144_周坤_7_13.c
@@ -0,0 +1,22 @@
+# Homework7
+#include<stdio.h>
+int main()
+{
+  float P(int n,int x);
+	int x,n;
+	printf("请输入阶数n=");
+	scanf("%d",&n);
+	printf("请输入自变量x=");
+	scanf("%d",&x);
+	printf("%.2f\n",P(n,x));
+	return 0;
+}
+float P(int n,int x)
+{
+	if(n==0)
+		return 1;
+	else if(n==1)
+		return x;
+	else
+		return ((2*n-1)*x-P((n-1),x)-(n-1)*P((n-2),x))/n;
+}
