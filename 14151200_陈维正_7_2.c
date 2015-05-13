b1e1eb1541d4ce530bc25fc21f427e2627b79969
2015CLanguage <buaa_2015_c@163.com>
2015-05-13 18:39:10 +0800

    Create 14151200_陈维正_7_2.c

---
+++ 14151200_陈维正_7_2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+float f(double x,double y,double z);
+float g(double x,double y,double z);
+double a,b,c;
+printf("请输入三个数\n");
+scanf("%lf%lf%lf",&a,&b,&c);
+if((b*b-4*a*c)>0)
+	f(a,b,c);
+if((b*b-4*a*c)==0)
+	g(a,b,c);
+if((b*b-4*a*c)<0)
+	printf("方程无解");
+return 0;
+}
+
+float f(double x,double y,double z)
+{
+double p,r1,r2;
+p=y*y-4*x*z;
+r1=(-y+pow(p,1/2))/(2*x);
+r2=(-y-pow(p,1/2))/(2*x);
+printf("%lf,%lf",r1,r2);
+
+}
+
+float g(double x,double y,double z)
+{
+double r;
+r=-y/2*x;
+printf("%lf",r);
+}
