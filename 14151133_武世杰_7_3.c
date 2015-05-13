00c74d030e7e89c9d23f01a33b5da68e6f5bdf00
2015CLanguage <buaa_2015_c@163.com>
2015-05-13 23:56:56 +0800

    Create 14151133_武世杰_7_3.c

---
+++ 14151133_武世杰_7_3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int prime_number(int);
+	int n;
+	printf("请输入一个数：");
+	scanf_s("%d", &n);
+	if (prime_number(n))
+		printf("%d是一个素数。\n", n);
+	else
+		printf("%d不是素数。\n", n);
+	return 0;
+}
+int prime_number(int n)
+{
+	int i,a=1;
+	if (n == 1)
+		a = 0;
+	for (i = 2; i <= n / 2 && a == 1; i++)
+	{
+		if (n%i == 0)
+			a = 0;
+	}
+	return(a);
+}
