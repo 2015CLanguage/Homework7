607486f596b5d33c2a88c9554e46dbef5036f788
2015CLanguage <buaa_2015_c@163.com>
2015-05-15 12:30:41 +0800

    Create 14151162_郑崔磊_7_13.c

---
+++ 14151162_郑崔磊_7_13.c
@@ -0,0 +1,23 @@
+Enter file contents here
+#include<stdio.h>
+float pn(int n,float x);
+float pn(int n,float x)
+{
+   if(n==0)
+     return (1);
+   else if(n==1)
+     return (x);
+   else
+     return ((2*n-1)*x-pn((n-1),x)-(n-1)*pn((n-2),x)/n);
+}
+int main()
+{
+  int n;
+  float x,p;
+  printf("input n,x:\n");
+  scanf("%d%f",&n,&x);
+  p=pn(n,x);
+  printf("P%d(%f)=%6.2f\n",n,x,p);
+  return 0;
+
+}
