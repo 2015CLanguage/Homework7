227bfbd19e9f9b7b855d8397f65cb15801576569
2015CLanguage <buaa_2015_c@163.com>
2015-05-16 20:11:47 +0800

    Create 14151219_柳寅_7_3.c

---
+++ 14151219_柳寅_7_3.c
@@ -0,0 +1,22 @@
+# include<stdio.h> 
+void f1(int x) 
+{ 
+   int i,j=0; 
+   for(i=1;i<=x;i++) 
+{ 
+if(x%i==0) 
+j=j+1; 
+} 
+if(j>2) 
+	printf("然而这并不是一个素数:）\n"); 
+else 
+    printf("这个是一个素数:）\n"); 
+} 
+int main(void) 
+{ 
+int m; 
+printf("请输入一个整数：\n"); 
+scanf("%d",&m); 
+f1(m); 
+return 0; 
+} 
