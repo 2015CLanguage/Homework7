bedc796832be09a1a09de7f2d095155c9b5c196f
2015CLanguage <buaa_2015_c@163.com>
2015-05-16 13:17:35 +0800

    Create 14151167_刘明伟_7_6.c

---
+++ 14151167_刘明伟_7_6.c
@@ -0,0 +1,25 @@
+Enter file contents here
+# include <stdio.h>
+int main()
+{
+	int lmw(char a[], char b[]);
+	char c[30], d[30], e[60];
+	printf("请输入一段字符：");
+	gets_s(c);
+	printf("请输入另一段字符：");
+	gets_s(d);
+	e[60] = lmw(c, d);
+	return 0;
+}
+int lmw(char a[], char b[])
+{
+	char f[60];
+	int i, j;
+	for (i = 0; a[i] != '\0'; i++)
+		 f[i] = a[i];
+	for (j = i; b[j-i ] != '\0'; j++)
+		f[j] = b[j -i];
+	f[j] = '\0';
+	printf("%s", &f);
+	return (f[60]);
+}
