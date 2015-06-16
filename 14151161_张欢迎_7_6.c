c5c382f72e0022db4efc52aaf2b6feb31d5831a1
2015CLanguage <buaa_2015_c@163.com>
2015-06-16 13:37:03 +0800

    Create 14151161_张欢迎_7_6.c

---
+++ 14151161_张欢迎_7_6.c
@@ -0,0 +1,21 @@
+#include "stdio.h" 
+ #include "string.h" 
+ 
+ 
+ int main() 
+ { 
+     char s[10],t[10],u[20]; 
+     int a,b,i; 
+     printf("请输入两个字符串：\n字符串1="); 
+     gets(s); 
+     printf("字符串2="); 
+     gets(t); 
+     b=strlen(t); 
+     a=strlen(s); 
+     for(i=0;i<20;i++){u[i]='\0';} 
+     for(i=0;i<a;i++){u[i]=s[i];} 
+     for(i=a;i<a+b;i++){u[i]=t[i-a];} 
+     printf("连接后的字符串为："); 
+     puts(u); 
+     return 0; 
+ } 
